e89566dc9b9a28ab3b81c8317f02094497eb35c4
Vasavi K <vasavikasireddi@gmail.com>
2026-06-13 00:28:21 +0530

    frontend url changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 
 const API = "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const sendMessage = (message) =>
-  axios.post(`${API}/chat`, { message });
+  axios.post(`${API_URL}/chat`, { message });
 
 export const uploadFile = (file) => {
   const formData = new FormData();
   formData.append("file", file);
 
-  return axios.post(`${API}/upload`, formData, {
+  return axios.post(`${API_URL}/upload`, formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
